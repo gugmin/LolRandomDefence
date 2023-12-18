7d1c81f91f0b2626cf1b795cca5173b5ce2d8f0a
gugmin <149454489+gugmin@users.noreply.github.com>
2023-12-18 14:07:03 +0900

    addHealthBar

--- Assets/03.ScriptableObject/Enemy/Scripts/Enemy.cs
+++ Assets/03.ScriptableObject/Enemy/Scripts/Enemy.cs
@@ -1,15 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+
+[System.Serializable]
+public class Condition
+{
+    [HideInInspector]
+    public float curValue;
+    public float maxValue;
+    public Image uiBar;
+
+
+    public float GetPercentage()
+    {
+        return curValue / maxValue;
+    }
+
+}
+
 
 public class Enemy : MonoBehaviour
 {
+    public Condition health;
+
+    private void Start()
+    {
+        health.maxValue = GameManager.instance.enemyStats.enemyHealth;
+        health.curValue = health.maxValue;
+    }
+
     private void Update()
     {
-        if (GameManager.instance.enemyStats.enemyHealth == 0) 
+        //юс╫ц
+        health.curValue -= Time.deltaTime;
+        if (health.curValue <= 0f) 
         {
             EnemyDie();
         }
+
+        health.uiBar.fillAmount = health.GetPercentage();
     }
 
     void EnemyDie()
@@ -21,6 +52,7 @@ public class Enemy : MonoBehaviour
         {
             GameManager.instance.StartCoroutine("StartRound");
         }
-        gameObject.SetActive(false);
+        Destroy(gameObject);
+        //gameObject.SetActive(false);
     }
 }
